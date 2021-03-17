a3452f1c61b13aab6992a75117f4671a1441369d
Akumai <drago@bk.ru>
2021-03-17 19:52:48 +0300

    tailwind config

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div class="App">
-      <header class="App-header">
-        <img src={logo} class="App-logo" alt="logo" />
+    <div class="text-center">
+      <div class="app-header min-h-screen flex flex-col items-center justify-center text-white text-2xl">
+      
+        <img src={logo} class="h-40" alt="logo" />
         <p>
           Let's try creating our tiny cards!
         </p>
-      </header>
+      </div>
     </div>
   );
 }
