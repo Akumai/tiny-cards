bc40252e904cec0cf49a84b5a9589f39410a81fd
nuarat <natalielielie@gmail.com>
2021-03-18 23:17:45 +0300

    1. Update the background colour. 2. Add 4 hardcoded rows for decks.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ function App() {
       <div class="app-header min-h-screen flex flex-col items-center justify-center text-white text-2xl">
       
         <img src={logo} class="h-40" alt="logo" />
-        <p>
-          Let's try creating our tiny cards!
-        </p>
+
+              <div>New words</div>
+              <div>Kanji</div>
+              <div>Verbs and conjugations</div>
+              <div>Topic: food</div>
+
       </div>
     </div>
   );
